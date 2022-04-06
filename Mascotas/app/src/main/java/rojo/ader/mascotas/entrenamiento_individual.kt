0254ee45414f92bdd466b277cf7baa70a543f71c
RojoAder <85540382+RojoAder@users.noreply.github.com>
2022-04-06 16:55:45 -0500

    Varias pantallas hechas

---
+++ Mascotas/app/src/main/java/rojo/ader/mascotas/entrenamiento_individual.kt
@@ -0,0 +1,11 @@
+package rojo.ader.mascotas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class entrenamiento_individual : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_entrenamiento_individual)
+    }
+}
---
+++ Mascotas/app/src/main/java/rojo/ader/mascotas/foroMenu.kt
@@ -0,0 +1,11 @@
+package rojo.ader.mascotas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class foroMenu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_foro_menu)
+    }
+}
